c4a5b38e296d583a912adaa423a0db12d7459705
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-09-19 15:45:57 +0800

    Create 4-1_pizza.py

---
+++ Chapter4/4-1_pizza.py
@@ -0,0 +1,5 @@
+pizzas=['New York Style','Chicago Style','California Style','Pan Pizza Style','Thick Style']
+for pizza in pizzas:
+    print(pizza)
+    print(pizza+", I like pepperoni pizza")
+print("I really like pizza!")
