940be5f7c6a2343bbecf08eaedcbb5b1acd5ae02
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-09-19 16:47:54 +0800

    Create 4-6_oddnumber.py

---
+++ Chapter4/4-6_oddnumber.py
@@ -0,0 +1,11 @@
+oddnum=[(2*value+1) for value in range(0,10)]
+print(oddnum)
+
+oddnum1=list(range(1,20,2))
+print(oddnum1)
+
+oddnum2=[1,3,5,7,9,11,13,15,17,19]
+for value in oddnum2:
+    print(value)
+    
+    
