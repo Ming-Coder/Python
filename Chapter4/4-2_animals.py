5ca877619efcad0e423d4a8b858a9269da64f6a1
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-09-19 15:53:55 +0800

    Create 4-2_animals.py

---
+++ Chapter4/4-2_animals.py
@@ -0,0 +1,6 @@
+animals=['dog','cat','bird','rabbit','hamster']
+for animal in animals:
+    print(animal)
+    print("A "+animal+" would make a great pet")
+print("Any of these animals would make a great pet!")
+
