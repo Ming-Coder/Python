a48517709702477bd18ed49d466c699eb7ff4f95
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-11-24 21:56:16 +0800

    Update 4-11.py

--- Chapter4/4-11.py
+++ Chapter4/4-11.py
@@ -1,5 +1,17 @@
-pizzas=['New York Style','Chicago Style','California Style','Pan Pizza Style','Thick Style']
-for pizza in my_pizzas:
-    print(pizza)
-    print(pizza+", I like pepperoni pizza")
-print("I really like pizza!")
+my_pizzas=['New York Style','Chicago Style','California Style','Pan Pizza Style','Thick Style']
+friend_pizzas=my_pizzas[:]
+
+print(my_pizzas[:])
+print(friend_pizzas[:])
+
+my_pizzas.append('US Pizssa')
+friend_pizzas.append('UK Pizssa')
+
+print("My favorite pizzas are:")
+print(my_pizzas)
+print("My friend's favorite pizzas are:")
+print(friend_pizzas)
+
+for friend_pizza in friend_pizzas:
+    print(friend_pizza)
+ 
