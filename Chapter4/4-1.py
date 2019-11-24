57ed4259e3a3745467484b2018d6301d2e0e276e
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-11-24 21:39:54 +0800

    Create 4-1.py

---
+++ Chapter4/4-1.py
@@ -0,0 +1,10 @@
+ch=['a','b','c','d','e','f','g','h','i']
+print("The first three items in the list are:")
+print(ch[:3])
+
+print("Three items from the middle of the list are:")
+print(ch[3:6])
+
+print("The last three items in the list are:")
+print(ch[-3:])
+
