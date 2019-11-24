f5d4699b4bfd9b40564a08f403a633fcc3bd63bd
Ming-Coder <42361808+Ming-Coder@users.noreply.github.com>
2019-11-24 22:05:13 +0800

    Create 4-12.py

---
+++ Chapter4/4-12.py
@@ -0,0 +1,11 @@
+my_foods=['pizza','falafel','carrot','cake']
+friend_foods=my_foods[:]
+
+print("My faorite foods are:")
+for my_food in my_foods:
+    print(my_food)
+
+print("\nMy friend's favorite foods are:")
+for friend_food in friend_foods:
+    print(friend_food)
+
